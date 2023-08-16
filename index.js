028cebaeacb94fc6925d2a89b2693cc29bb3aaeb
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-08-16 20:15:18 +0200

    .

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { Command } = require("commander");
+const program = new Command();
 const {
   listContacts,
   getContactById,
@@ -5,18 +7,50 @@ const {
   addContact,
 } = require("./contacts");
 
-console.time("listContacts");
-listContacts();
-console.timeEnd("listContacts");
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-console.time("getContactById");
-getContactById(2);
-console.timeEnd("getContactById");
+program.parse(process.argv);
 
-console.time("addContact");
-addContact("Nowa Osoba", "nowa@example.com", "123456789");
-console.timeEnd("addContact");
+const argv = program.opts();
 
-console.time("removeContact");
-removeContact(3);
-console.timeEnd("removeContact");
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      if (id) {
+        getContactById(id);
+      } else {
+        console.log("Please provide a valid ID.");
+      }
+      break;
+
+    case "add":
+      if (name && email && phone) {
+        addContact(name, email, phone);
+      } else {
+        console.log("Please provide name, email, and phone.");
+      }
+      break;
+
+    case "remove":
+      if (id) {
+        removeContact(id);
+      } else {
+        console.log("Please provide a valid ID.");
+      }
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
